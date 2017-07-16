510501ac0b9c0f5f055fa4e31c722b8e1555148b
Chengchi Zhou <zcc.qwer@gmail.com>
2017-07-16 22:16:43 +0800

    led

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
 function addLED() {
     for (var i = 1; i <= 16; i++) {
         for (var j = 1; j <= 25; j++) {
-            $('.led-table').append("<div class='led-cube' id='cube-" + String(i) + String(j) + "'>");
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    if (this.resoponseText == 0)
-                        $("#cube-"+String(i)+String(j)).addClass("white-cube");
-                }
-            }
-            xmlhttp.open("GET", "/php/getled.php?r="+String(i)+"&c="+String(j), true);
-            xmlhttp.send();
+            $('.led-table').append("<div class='led-cube white-cube' id='cube-" + String(i) + "-" + String(j) + "'>");
         }
     }
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var text = this.responseText.split(' ');
+			for (var i = 0; i < text.length / 3; i++)
+				if (String(text[i * 3 + 2]) == "1")
+					$("#cube-"+String(text[i * 3])+"-"+String(text[i * 3 + 1])).removeClass("white-cube");
+		}
+	}
+	xmlhttp.open("GET", "/php/getled.php?action=build", true);
+	xmlhttp.send();
+
     $(".led-cube").click(function() {
-        if ($(this).hasClass("white-cube"))
+        if ($(this).hasClass("white-cube")) {
             $(this).removeClass("white-cube");
-        else
+			var clickLED = new XMLHttpRequest();
+			clickLED.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+				}
+			}
+			var id = $(this).attr('id').split('-');
+			clickLED.open("GET", "/php/getled.php?action=modify&r="+id[1]+"&c="+id[2]+"&t=1", true);
+			clickLED.send();
+		}
+        else {
             $(this).addClass("white-cube");
+			var clickLED = new XMLHttpRequest();
+			clickLED.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+				}
+			}
+			var id = $(this).attr('id').split('-');
+			clickLED.open("GET", "/php/getled.php?action=modify&r="+id[1]+"&c="+id[2]+"&t=0", true);
+			clickLED.send();
+		}
     });
 }
 
--- php/getled.php
+++ php/getled.php
@@ -1,15 +1,25 @@
 <?php
-$r = intval($_GET['r']);
-$c = intval($_GET['c']);
+$action = $_GET['action'];
 $con = mysqli_connect('localhost', 'root', 'MysqlRooT', 'ohnaive');
 if (!$con) {
-    die("Could not connect.");
+	die("Could not connect.");
 }
 mysqli_select_db($con, "ohnaive");
-$sql = "SELECT * FROM user WHERE row=".$r." AND col=".$c;
-$result = mysqli_query($con, $sql);
-while ($row = mysqli_fetch_array($result)) {
-    echo $row['type'];
+
+if ($action == "build") {
+	$sql = "SELECT * FROM led";
+	$result = mysqli_query($con, $sql);
+	while ($row = mysqli_fetch_array($result)) {
+		echo $row['row']." ".$row['col']." ".$row['type']." ";
+	}
 }
+if ($action == "modify") {
+	$r = intval($_GET['r']);
+	$c = intval($_GET['c']);
+	$t = intval($_GET['t']);
+	$sql = "UPDATE led SET type=".$t." WHERE row=".$r." AND col =".$c;
+	$result = mysqli_query($con, $sql);
+}
+
 mysqli_close($con);
 ?>
